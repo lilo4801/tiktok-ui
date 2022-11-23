165db14be2063bf91ce69662c1789de8d97ade24
muoi.nguyenvanminh <lilo4801>
2022-11-23 13:18:52 +0700

    add css header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,44 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner, faPlus, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
+import styles from './header.module.scss'
+import images from '~/assets/images';
+
+const cx = classNames.bind(styles);
 
 const Header = () => {
-    return <h2>header</h2>
+    return <header className={cx('wrapper')}>
+        <div className={cx('inner')}>
+            <img src={images.logo} alt="Tiktok" />
+            <div className={cx('search')}>
+                <input placeholder="Search accounts and videos" spellCheck={false} />
+                <button className={cx('clear')}>
+                    <FontAwesomeIcon icon={faCircleXmark} />
+                </button>
+                {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+
+                <button className={cx('search-btn')}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+            </div>
+            <div className={cx('actions')}>
+                <button className={cx('upload-btn')}>
+
+                    <Link to='/upload'><FontAwesomeIcon icon={faPlus} />
+                        <span>Upload</span></Link>
+
+                </button>
+                <button className={cx('login-btn')}>
+                    Log In
+                </button>
+                <div className={cx('setting')}>
+                    <FontAwesomeIcon icon={faEllipsisVertical} />
+                </div>
+            </div>
+        </div>
+    </header>
 }
 
 export default Header;
